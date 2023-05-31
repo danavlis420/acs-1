f29ef872cadd400f276012b0de8edf174f3494fb
danavlis420 <silva.adaniel.01@gmail.com>
2023-05-31 10:53:14 -0300

    Vuelvo a la versión del primer ACS del sketch

--- sketch.js
+++ sketch.js
@@ -1,27 +1,13 @@
 //obra original:
 //https://www.wassilykandinsky.net/images/works/518.jpg?version=7
 //colores
-let a1 = "#CB655B";
-let a2 = "#E9D8A6";
-let a3 = "#9B2226";
-let a4 = "#CA6702";
-let a5 = "#EE9B00";
-let a6 = "#005F73";
-let a7 = "#001219";
-let b1 = "#2b0948";
-let b2 = "#461846";
-let b3 = "#612844";
-let b4 = "#7d3742";
-let b5 = "#98463f";
-let b6 = "#b3563d";
-let b7 = "#ce653b";
-let col1 = a1;
-let col2 = a2;
-let col3 = a3;
-let col4 = a4;
-let col5 = a5;
-let col6 = a6;
-let col7 = a7;
+let col1 = "#CB655B";
+let col2 = "#E9D8A6";
+let col3 = "#9B2226";
+let col4 = "#CA6702";
+let col5 = "#EE9B00";
+let col6 = "#005F73";
+let col7 = "#001219";
 
 function setup() {
   frameRate(24);
@@ -73,21 +59,4 @@ function draw() {
   rect(70, 335, 45, 15);
   fill(col6);
   ellipse(60, 270, 65);
-  if (mouseIsPressed == true) {
-    col1 = b1;
-    col2 = b2;
-    col3 = b3;
-    col4 = b4;
-    col5 = b5;
-    col6 = b6;
-    col7 = b7;
-  } else {
-    col1 = a1;
-    col2 = a2;
-    col3 = a3;
-    col4 = a4;
-    col5 = a5;
-    col6 = a6;
-    col7 = a7;
-  }
 }
